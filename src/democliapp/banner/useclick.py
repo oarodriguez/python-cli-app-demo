be6ff9fce3247639d88c7ac269c2c72c08f89014
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2023-03-05 17:05:35 -0600

    Enable a bit of rich style on the banner in the `click`-based CLI.

--- src/democliapp/banner/useclick.py
+++ src/democliapp/banner/useclick.py
@@ -2,20 +2,47 @@
 from typing import Optional
 
 from click import Group, argument, option
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
 
 from .banner import Banner
 
 # Initialize the object that enables the CLI.
 app = Group()
 
+# Rich console object. We use it to enrich the terminal output.
+console = Console()
+
 
 @app.command()
 @argument("headline", type=str)
 @option("--font", type=str, help="Suggest a font for the banner text.")
-def show_banner(headline: str, font: Optional[str] = None):
+@option(
+    "--headline-style",
+    type=str,
+    help="Suggest a style to apply to the banner headline.",
+    default=None,
+)
+@option(
+    "--with-panel",
+    is_flag=True,
+    default=False,
+    help="Add a panel around the headline.",
+)
+def show_banner(
+    headline: str,
+    font: Optional[str] = None,
+    headline_style: Optional[str] = None,
+    with_panel: bool = True,
+):
     """Show a fancy banner with a HEADLINE using ASCII art."""
     banner = Banner(headline=headline, font_name=font)
-    print(banner.text)
+    banner_content = Text(banner.text)
+    banner_content.stylize(style=headline_style)
+    if with_panel:
+        banner_content = Panel(banner_content)
+    console.print(banner_content)
 
 
 if __name__ == "__main__":
